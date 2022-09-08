f170ecedca14136569afea074260c711dd115e77
allenkays <allenkays@tutanota.com>
2022-09-08 14:51:48 -0400

    Betty Style corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s) \n Size of an int: %d byte(s)\n Size of a long int: %d byte(s)\n Size of a long long int: %d byte(s)\n Size of a float: %d byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float))
+	printf("Size of a char: %d byte(s) \n Size of an int: %d byte(s)\n Size of a long int: %d byte(s)\n Size of a long long int: %d byte(s)\n Size of a float: %d byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
 	return (0);
 }
