3f7c47ee1ed7a63d97cf55daa9692f0a9864afd6
allenkays <allenkays@tutanota.com>
2022-09-08 15:48:12 -0400

    advanced system and intel tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints text to standard error
+ * Return: 0 on success
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
