a7aa70655ca428242e46af06e3ca06f8a8fb4552
allenkays <allenkays@tutanota.com>
2022-09-08 16:03:18 -0400

    fprintf to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
