97d6277a44440fd464f1826bc808bdbf393b35e7
allenkays <allenkays@tutanota.com>
2022-09-22 10:57:12 -0400

    Enclosewq loop blocks

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,13 @@ char *string_toupper(char *s)
 	int i = 0;
 
 	while (*(s + i))
+	{
 		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
 			(*(s + i) -= 'a' - 'A');
-			 i++;
 		}
+		i++;
+	}
+
 	return (s);
 }
