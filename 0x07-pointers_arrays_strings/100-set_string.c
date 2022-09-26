b9cd05f642c23a6d777210e90c4b68146e980e5b
allenkays <allenkays@tutanota.com>
2022-09-26 17:09:35 -0400

    fix prototype

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,7 +2,7 @@
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: pointer to a pointer 
+ * @s: pointer to a pointer
  * @to: pointer to char
  */
 void set_string(char **s, char *to)
