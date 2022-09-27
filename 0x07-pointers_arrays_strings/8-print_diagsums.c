b83beddf72649036977f8bc5f7742caa01ae1475
allenkays <allenkays@tutanota.com>
2022-09-27 09:50:45 -0400

    initilize sum of diagonals in declartion

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,14 +8,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-	unsigned int sum, sum1;
+	unsigned int sum, sum1 = 0;
 	int i, pos, pos1;
 
 	for (i = 0; i < size; i++)
 	{
-		pos = (size * i) + i;
+		pos = (i * size) + i;
 		sum += *(a + pos);
-		pos1 = (size * i) + (size - 1 - i);
+		pos1 = (i * size) + (size - 1 - i);
 		sum1 += *(a + pos1);
 	}
 	printf("%i, %i\n", sum, sum1);
