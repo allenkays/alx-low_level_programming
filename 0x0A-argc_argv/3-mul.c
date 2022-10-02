c8bb53514e9ae89a5dff0fbacb79c9ac860c9ffb
allenkays <allenkays@tutanota.com>
2022-10-02 08:33:00 -0400

    Fix seg fault

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,23 +1,25 @@
-#include <stdlib.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - program multiplies two numbers passed to it from commandline
- * @argc: arguments count
- * @argv: arguments vector
- * Return: 0 on success, non-zero otherwise.
+ * main - multiplies two numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
  */
 int main(int argc, char *argv[])
 {
 	int a, b, c;
 
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
 	if (argc != 3)
 	{
 		puts("Error");
 		return (1);
 	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 	c = a * b;
 	printf("%d\n", c);
 	return (0);
