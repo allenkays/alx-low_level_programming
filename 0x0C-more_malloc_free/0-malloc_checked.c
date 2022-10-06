27e6d890d7f6289cbf4af7c4117a8c81069122be
allenkays <allenkays@tutanota.com>
2022-10-06 07:30:34 -0400

    First commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: amount of memory to allocate
+ * Return: pointer to the allocated space; or 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *a = malloc(b * sizeof(int));
+
+	if (a == NULL)
+	{
+		exit(98);
+	}
+	else 
+	{
+		return (a);
+	}
+	free(a);
+}
