9ee03b12facee61cadf78290ce34b9d2a44c8b41
allenkays <allenkays@tutanota.com>
2022-10-11 09:18:58 -0400

    create new instance of dog struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,13 +26,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog->name == NULL)
 	{
 		free(dog);
+
 		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * (len1 + 1));
 	if (dog->owner == NULL)
 	{
-		free(dog);
 		free(dog->name);
+		free(dog);
+
 		return (NULL);
 	}
 	str_copy(dog->name, name);
