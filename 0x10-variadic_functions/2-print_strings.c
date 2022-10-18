d40758145912392a442bb3864735a5320d06ff46
allenkays <allenkays@tutanota.com>
2022-10-18 03:10:43 -0400

    Print variable number of input string params

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,11 +14,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *s;
 
 	va_start(a, n);
-
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(a, char *);
-		if (s == NULL)
+		if (!s)
 			s = "nil";
 		printf("%s", s);
 		if (separator != NULL && i < n - 1)
