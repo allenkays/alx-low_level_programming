ef3a068cb8aedb5edbd0405fe8a3a3e12e18e883
allenkays <allenkays@tutanota.com>
2022-10-18 03:05:49 -0400

    Print variable number of input string params

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - outputs given srings with given delimiter
+ * @separator: delimiter between intergers
+ * @n: number of intergers passed to function
+ * Return: sum of all the params
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list a;
+	unsigned int i;
+	char *s;
+
+	va_start(a, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(a, char *);
+		if (s == NULL)
+			s = "nil";
+		printf("%s", s);
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(a);
+}
