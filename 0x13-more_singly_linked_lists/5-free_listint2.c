ae85f937e8d32ea965fbe8f3f8acff596125d7e6
allenkays <allenkays@tutanota.com>
2022-10-27 17:16:13 -0400

    Modify free order

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,16 +10,13 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *cursor;
-	listint_t **temp = head;
 
-	if (temp != NULL)
+	if (head == NULL)
+		return;
+	while (*head != NULL)
 	{
-		while (*head != NULL)
-		{
-			cursor = *head;
-			free(cursor);
-			*head = (*head)->next;
-		}
-		*temp = NULL;
+		cursor = (*head)->next;
+		free(*head);
+		*head = cursor;
 	}
 }
