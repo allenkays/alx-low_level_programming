1cb90f4a0569b18ce3cab55de167c0a0447170b5
allenkays <allenkays@tutanota.com>
2022-10-27 16:39:16 -0400

    task 0- print linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-const listint_t *cursor = h;
-size_t count = 0;
+	size_t node = 0;
 
-while (cursor != NULL)
-{
-printf("%d\n", cursor->n);
-count += 1;
-cursor = cursor->next;
-}
-return (count);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		node += 1;
+		h = h->next;
+	}
+	return (node);
 }
