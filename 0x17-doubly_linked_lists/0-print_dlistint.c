c3e1e3ebf8248652c4a8660b5d65b611718dcab4
allenkays <allenkays@tutanota.com>
2022-12-11 00:42:05 -0500

    edit double declaration of h

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,13 +7,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *temp = h;
 	size_t n = 0
 
-	while (temp != NULL)
+	while (h)
 	{
-		printf("%d", temp->data);
-		temp = temp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		n++;
 	}
 	return (n)
