5b498d87d03ca739effff0ac7a4f6526756638b7
allenkays <allenkays@tutanota.com>
2022-12-11 01:30:53 -0500

    count and return elements in doubly-linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements a dlistint_t list
+ * @h: pointer to double-linked list
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
