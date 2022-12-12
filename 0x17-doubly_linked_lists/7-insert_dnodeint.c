ea133902910bb63b64013b6ea16c574b625fae60
allenkays <allenkays@tutanota.com>
2022-12-12 02:05:52 -0500

    change loop to for loop and remove space on line 19

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,13 +14,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (idx)
+	for (; idx != 1; idx--)
 	{
-		
 		tmp = tmp->next;
 		if (tmp == NULL)
 			return (NULL);
-		idx--;
 	}
 
 	if (tmp->next == NULL)
