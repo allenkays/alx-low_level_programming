6a144cb0ea38e7022644aaaa7a42685658c331ef
allenkays <allenkays@tutanota.com>
2022-12-11 20:05:18 -0500

    add asterisk to pointer variable declaration

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -6,16 +6,16 @@
  * @n: interger to add to doubly linked list
  * Return: the address of the new element, or NULL if it failed
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new, end
+	dlistint_t *new, *end;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	new->next = NULL;
+
 	if (*head == NULL)
 	{
 		new->prev = NULL;
