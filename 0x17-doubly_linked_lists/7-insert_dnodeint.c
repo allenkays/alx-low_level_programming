f7d0906cf2388ce0efc02af4c57bfad5969c43fd
allenkays <allenkays@tutanota.com>
2022-12-12 02:01:07 -0500

    edit index loop

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,10 +16,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	while (idx)
 	{
-		idx--;
+		
 		tmp = tmp->next;
 		if (tmp == NULL)
 			return (NULL);
+		idx--;
 	}
 
 	if (tmp->next == NULL)
