12f562ea59b92bbe3bb7bda8344dd2df43873c46
allenkays <allenkays@tutanota.com>
2023-04-13 09:32:41 -0400

    Jump algorithm implementation

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - Searches for a value in a sorted array of integers using the
+ *               Jump search algorithm
+ *
+ * @array: A pointer to the first element of the array to search in
+ * @size: The number of elements in the array
+ * @value: The value to search for
+ *
+ * Return: The first index where value is located, or -1 if not found
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	unsigned int i, jump, left, right;
+	
+	if (array == NULL || size == 0)
+		return (-1);
+	jump = 0;
+	while (jump * jump < size)
+		jump++;
+
+	left = 0;
+	right = jump;
+
+	while (right < size && array[right] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", right, array[right]);
+		left = right;
+		right += jump;
+	}
+
+	for (i = left; i <= right && i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
